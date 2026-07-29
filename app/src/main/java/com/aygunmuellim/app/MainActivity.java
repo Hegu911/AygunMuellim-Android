cf4cf0284c3ae2911ff9718a983b7c96572e07fb
Hegu911 <hegu911@github.com>
2026-07-29 15:00:22 +0400

    Simplify app: pure WebView, no splash, minimal code

--- app/src/main/java/com/aygunmuellim/app/MainActivity.java
+++ app/src/main/java/com/aygunmuellim/app/MainActivity.java
@@ -1,34 +1,23 @@
 package com.aygunmuellim.app;
 
 import android.os.Bundle;
-import android.view.View;
-import android.webkit.WebChromeClient;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
-import android.widget.FrameLayout;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
     private WebView webView;
-    private FrameLayout splash;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        splash = findViewById(R.id.splash);
         webView = findViewById(R.id.webview);
-
-        getWindow().getDecorView().setSystemUiVisibility(
-            View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
-            | View.SYSTEM_UI_FLAG_FULLSCREEN
-            | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-        );
-
         WebSettings settings = webView.getSettings();
+
         settings.setJavaScriptEnabled(true);
         settings.setSupportZoom(false);
         settings.setBuiltInZoomControls(false);
@@ -36,29 +25,8 @@ public class MainActivity extends AppCompatActivity {
         settings.setLoadWithOverviewMode(true);
         settings.setUseWideViewPort(true);
 
-        android.webkit.CookieManager.getInstance().setAcceptCookie(true);
-        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.LOLLIPOP) {
-            android.webkit.CookieManager.getInstance().setAcceptThirdPartyCookies(webView, true);
-        }
-
         webView.setWebViewClient(new WebViewClient());
-        webView.setWebChromeClient(new WebChromeClient() {
-            @Override
-            public void onProgressChanged(WebView view, int newProgress) {
-                if (newProgress >= 80 && splash.getVisibility() == View.VISIBLE) {
-                    fadeOutSplash();
-                }
-            }
-        });
-
         webView.loadUrl("https://lms-2hd.pages.dev");
-    }
-
-    private void fadeOutSplash() {
-        splash.animate()
-            .alpha(0f)
-            .setDuration(500)
-            .withEndAction(() -> splash.setVisibility(View.GONE));
     }
 
     @Override
